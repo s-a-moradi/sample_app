f010c5a27d47367d771cc27a1467b045ab000749
sam <s.a.moradi@ut.ac.ir>
2014-05-01 21:58:27 +0430

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,31 +2,38 @@ require 'spec_helper'
 
 describe User do
 
-  before do
+    before do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
-
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
-
-  it { should be_valid }
-  it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
-  it { should respond_to(:microposts) }
-  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
 
-  describe "with admin attribute set to 'true'" do
+  it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+describe "with admin attribute set to 'true'" do
     before do
       @user.save!
       @user.toggle!(:admin)
@@ -39,18 +46,12 @@ describe User do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-
-  describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when name is too long" do
+describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
-  describe "when email format is invalid" do
+ describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -70,17 +71,25 @@ describe User do
       end
     end
   end
-
-  describe "when email address is already taken" do
+describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
 
     it { should_not be_valid }
   end
+describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
-  describe "when password is not present" do
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
                        password: " ", password_confirmation: " ")
@@ -92,8 +101,7 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
-
-  describe "with a password that's too short" do
+describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
@@ -113,13 +121,12 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
-
-  describe "remember token" do
+describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
-
   describe "micropost associations" do
+
     before { @user.save }
     let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -131,7 +138,6 @@ describe User do
     it "should have the right microposts in the right order" do
       expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
     end
-
     it "should destroy associated microposts" do
       microposts = @user.microposts.to_a
       @user.destroy
@@ -140,16 +146,48 @@ describe User do
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
     end
-  
     describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
-    
-  end 
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,7 +51,6 @@ describe "Authentication" do
         end
 
         describe "after signing in" do
-
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
@@ -74,6 +73,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end        
       end
 
       describe "in the Microposts controller" do
@@ -88,8 +97,20 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
-    end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+      
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
